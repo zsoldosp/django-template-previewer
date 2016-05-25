7cbe2b79bf1d22d2bcb7fa3820d14f18d756f4f6
Peter <peter.zsoldos@gmail.com>
2016-05-25 10:41:51 +0200

    add names to all urls so we can easily test them

--- template_previewer/urls.py
+++ template_previewer/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('template_previewer.views',
-    url(r'^$', 'preview'),
+    url(r'^$', 'preview', name='preview'),
     url(r'^parse/$', 'parse', name='parse'),
-    url(r'^render/$', 'render'),
+    url(r'^render/$', 'render', name='render'),
 )

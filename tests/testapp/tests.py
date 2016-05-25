efe4cbc9737f371ec3748cf0afb366b4b01673a8
Peter <peter.zsoldos@gmail.com>
2016-05-25 10:42:10 +0200

    make sure we can load the main page

--- tests/testapp/tests.py
+++ tests/testapp/tests.py
@@ -1,7 +1,10 @@
-from django.test import TransactionTestCase
+from django.core.urlresolvers import reverse
+from django.test import TransactionTestCase, Client
 
 
 class RegressionTestCase(TransactionTestCase):
 
-    def test_hi(self):
-        self.fail('sdf')
+    def test_can_load_main_page(self):
+        url = reverse('preview')
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)

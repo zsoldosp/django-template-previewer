4aa4a3b3bf6ec9d9be42f9001c3ebc8509772d27
Peter <peter.zsoldos@gmail.com>
2016-05-25 11:06:51 +0200

    make sure we can parse the sample template example

--- tests/testapp/tests.py
+++ tests/testapp/tests.py
@@ -1,3 +1,4 @@
+import json
 from django.core.urlresolvers import reverse
 from django.test import TransactionTestCase, Client
 
@@ -8,3 +9,24 @@ class RegressionTestCase(TransactionTestCase):
         url = reverse('preview')
         response = self.client.get(url)
         self.assertEqual(200, response.status_code)
+
+    def test_can_load_parse_page_for_doc_sample_template(self):
+        url = reverse('parse') + '?template=sample.html'
+        response = self.client.get(url)
+        self.assertEqual(200, response.status_code)
+        data = json.loads(response.content)
+        self.maxDiff = None
+        self.assertEqual([
+                {u'name': u'title', u'children': [] },
+                {
+                    u'name': u'foo', u'children': [
+                        {
+                            u'name': u'bar', u'children': [
+                            {u'name': u'0', u'children': [
+                                {u'name': u'last_name', u'children': []},
+                                {u'name': u'first_name', u'children': []},
+                            ]},
+                        ]},
+                    ]
+                },
+            ], data)
